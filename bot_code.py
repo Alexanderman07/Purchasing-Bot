2e4616620626aa8f8e84fb9955b0d400ef651001
Alex Manjarrez <alexmanjarrez2000@gmail.com>
2022-07-26 15:17:28 -0700

    third commit

--- bot_code.py
+++ bot_code.py
@@ -5,6 +5,11 @@ def find_item(name):
     URL = ''
     html = requests.get(url=URL)
     term = json.loads(html.text)
-    print(term)
+    
+    for category in term['products_and_categories']:
+        for item in category:
+            if name in item['name']:
+                print(item['name'])
+                return item['id']
 
-find_item(None)
+find_item('Logo Split')
