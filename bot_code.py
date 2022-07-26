bd689b6b0bf012b7ec311774c57292faf653f523
Alex Manjarrez <alexmanjarrez2000@gmail.com>
2022-07-26 01:54:57 -0700

    second comit

--- bot_code.py
+++ bot_code.py
@@ -1,1 +1,10 @@
-from selenium import webdriver
+import requests
+import json
+
+def find_item(name):
+    URL = ''
+    html = requests.get(url=URL)
+    term = json.loads(html.text)
+    print(term)
+
+find_item(None)
