dbbc4a43dd51762f5ee696912e262d6131c2bcde
Alex Manjarrez <alexmanjarrez2000@gmail.com>
2022-07-27 01:22:18 -0700

    fourth commit

--- bot_code.py
+++ bot_code.py
@@ -7,9 +7,18 @@ def find_item(name):
     term = json.loads(html.text)
     
     for category in term['products_and_categories']:
-        for item in category:
+        for item in term['products_and_categories'][category]:
             if name in item['name']:
                 print(item['name'])
+                print(item['id'])
                 return item['id']
 
-find_item('Logo Split')
+def get_color(item_id, color):
+    URL = f'/{item_id}'
+    html = requests.get(url=URL)
+    output = json.loads(html.text)
+    print(output)
+
+if __name__ == '__main__':
+    item_id = find_item('Logo Split')
+    get_color(item_id, 'Black')
