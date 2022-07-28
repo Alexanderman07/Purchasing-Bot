352881a6fac754152a32784749f9421cc9658321
Alex Manjarrez <alexmanjarrez2000@gmail.com>
2022-07-28 16:32:07 -0700

    sixth commit

--- bot_code.py
+++ bot_code.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from selenium import webdriver
-
+from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome(executable_path='')
 
 def find_item(name):
